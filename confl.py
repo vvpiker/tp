9cee046357b3112c69e1ac91b6a0e3ae8e8e3380
vvpiker <vladi2029.er@gmail.com>
2024-09-25 00:36:41 +0300

    confl.py: run 'hello' with output to file

--- confl.py
+++ confl.py
@@ -3,3 +3,9 @@ print(common line)
 def hello(name: str) -> str:
     return f'Good day, {name}'
 
+if __name__ == '__main__':
+    name = input('Print your name here: ')
+    f = open('output.txt', 'w')
+    res = hello(name)
+    f.write(res)
+    f.close()
